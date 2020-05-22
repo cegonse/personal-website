9b10781d5332b91e3d449ab9d5f5c914003e312d
Cesar Gonzalez <cesar.gonzalez@casa-systems.com>
2020-05-22 14:38:34 +0200

    Implement mobile menu

---
+++ docs/src/main.js
@@ -0,0 +1,27 @@
+const toggleClass = (element, name) => {
+    if (element.classList.contains(name)) {
+        element.classList.remove(name)
+    } else {
+        element.classList.add(name)
+    }
+}
+
+
+const toggleMenu = () => {
+    const mobile_header = document.querySelector('#mobile-header')
+    const mobile_menu = document.querySelector('#mobile-menu')
+
+    toggleClass(mobile_menu, 'fa-rotate-90')
+    toggleClass(mobile_header, 'hidden')
+
+    for (i=0; i<mobile_header.children.length; ++i) {
+        toggleClass(mobile_header.children[i], 'hidden')
+    }
+}
+
+
+window.addEventListener('load', () => {
+    const mobile_menu = document.querySelector('#mobile-menu')
+
+    mobile_menu.addEventListener('click', toggleMenu)
+})
